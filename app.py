a3e5b2d97ad6f184917342a461bf597050491875
Jose <87710737+a2nis@users.noreply.github.com>
2023-07-21 16:59:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from socio_manager.socio_manager import socio_manager
 app = Flask(__name__)
 app.register_blueprint(home, url_prefix="/")
 app.register_blueprint(mercado, url_prefix="/mercado")
-app.register_blueprint(app_barcode, url_prefix="/app_barcode")
+#app.register_blueprint(app_barcode, url_prefix="/app_barcode")
 app.register_blueprint(socio_manager, url_prefix="/socio_manager")
 
 # Nivel de plano error 404
